69774e540e37b8856540e00d22113dd28d6f661c
koyak <keikotani926@gmail.com>
2009-01-31 14:08:37 +0000

    ・プロジェクト要素追加フィルターをカスタマイズ可能に ・設定ファイルの読み書き処理追加
    git-svn-id: https://www.seasar.org/svn/sandbox/koropokkur.net/trunk/koropokkur.net@11 34ecb7be-c3dc-40e9-8272-068907805f0b

---
+++ source/VSArrange/Config/ConfigFileManager.cs
@@ -0,0 +1,195 @@
+﻿#region Copyright
+/*
+ * Copyright 2005-2009 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace VSArrange.Config
+{
+    /// <summary>
+    /// 設定ファイルを読み書きするクラス
+    /// </summary>
+    public class ConfigFileManager
+    {
+        private const string SECTION_FILTERS = "filters";
+        private const string SECTION_FILTER = "filter";
+        private const string SUB_SECTION_FILE = "file";
+        private const string SUB_SECTION_FOLDER = "folder";
+        private const string ATTRIBUTE_FILTER_NAME = "name";
+        private const string ATTRIBUTE_FILTER_ENABLE = "enable";
+
+        /// <summary>
+        /// 設定ファイル読み込み
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static ConfigInfo ReadConfig(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentException("path");
+            }
+            //  初回起動時
+            if(!File.Exists(path))
+            {
+                return new ConfigInfo();
+            }
+    
+            XmlDocument configDocument = new XmlDocument();
+            configDocument.Load(path);
+            ConfigInfo configInfo = new ConfigInfo();
+            
+            //  フィルター情報
+            XmlElement filtersElement = configDocument[SECTION_FILTERS];
+            if (filtersElement != null)
+            {
+                XmlElement fileFilterElement = filtersElement[SUB_SECTION_FILE];
+                if(fileFilterElement != null)
+                {
+                    configInfo.FilterFileStringList = ReadConfigFilter(fileFilterElement);
+                }
+
+                XmlElement folderFilterElement = filtersElement[SUB_SECTION_FOLDER];
+                if (folderFilterElement != null)
+                {
+                    configInfo.FilterFolderStringList = ReadConfigFilter(folderFilterElement);
+                }
+            }
+
+            return configInfo;
+        }
+
+        /// <summary>
+        /// 設定ファイルからフィルター情報を読み込む
+        /// </summary>
+        /// <param name="filterElement"></param>
+        /// <returns></returns>
+        private static IList<ConfigInfoFilter> ReadConfigFilter(XmlElement filterElement)
+        {
+            IList<ConfigInfoFilter> returnFilterList = new List<ConfigInfoFilter>();
+            XmlNodeList filterNodeList = filterElement.GetElementsByTagName(SECTION_FILTER);
+            foreach (XmlNode filterNode in filterNodeList)
+            {
+                ConfigInfoFilter newFilter = new ConfigInfoFilter();
+                XmlAttribute nameAttr = filterNode.Attributes[ATTRIBUTE_FILTER_NAME];
+                if (nameAttr != null)
+                {
+                    newFilter.Name = nameAttr.Value;
+                }
+
+                XmlAttribute enableAttr = filterNode.Attributes[ATTRIBUTE_FILTER_ENABLE];
+                bool isEnable = false;
+                if (enableAttr != null && bool.TryParse(enableAttr.Value, out isEnable))
+                {
+                    newFilter.IsEnable = isEnable;
+                }
+
+                newFilter.FilterString = filterNode.InnerText;
+                returnFilterList.Add(newFilter);
+            }
+            return returnFilterList;
+        }
+
+        /// <summary>
+        /// 設定情報書き込み
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="configInfo"></param>
+        public static void WriteConfig(string path, ConfigInfo configInfo)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentException("path");
+            }
+
+            if(configInfo == null)
+            {
+                return;
+            }
+
+            XmlDocument configDocument = new XmlDocument();
+            XmlElement filtersElement = configDocument.CreateElement(SECTION_FILTERS);
+
+            //  ファイル用フィルター
+            XmlElement fileFiltersElement = CreateFilterSectionElement(
+                SUB_SECTION_FILE, configDocument, configInfo.FilterFileStringList);
+            if (fileFiltersElement != null)
+            {
+                filtersElement.AppendChild(fileFiltersElement);
+            }
+
+            //  フォルダ用フィルター
+            XmlElement folderFiltersElement = CreateFilterSectionElement(
+                SUB_SECTION_FOLDER, configDocument, configInfo.FilterFolderStringList);
+            if (folderFiltersElement != null)
+            {
+                filtersElement.AppendChild(folderFiltersElement);
+            }
+
+            configDocument.AppendChild(filtersElement);
+            configDocument.Save(path);
+        }
+
+        /// <summary>
+        /// フィルターセクションに書き込む内容を作成する
+        /// </summary>
+        /// <param name="subSectionName"></param>
+        /// <param name="configDocument"></param>
+        /// <param name="filters"></param>
+        /// <returns></returns>
+        private static XmlElement CreateFilterSectionElement(
+            string subSectionName, XmlDocument configDocument, IEnumerable<ConfigInfoFilter> filters)
+        {
+            if(filters == null)
+            {
+                return null;
+            }
+
+            if(string.IsNullOrEmpty(subSectionName))
+            {
+                throw new ArgumentNullException("subSectionName");
+            }
+
+            if(configDocument == null)
+            {
+                throw new ArgumentNullException("configDocument");
+            }
+
+            XmlElement filtersElement = configDocument.CreateElement(subSectionName);
+            foreach (ConfigInfoFilter filter in filters)
+            {
+                XmlElement filterElement = configDocument.CreateElement(SECTION_FILTER);
+
+                XmlAttribute filterNameAttr = configDocument.CreateAttribute(ATTRIBUTE_FILTER_NAME);
+                filterNameAttr.Value = filter.Name;
+                filterElement.Attributes.Append(filterNameAttr);
+
+                XmlAttribute filterEnableAttr = configDocument.CreateAttribute(ATTRIBUTE_FILTER_ENABLE);
+                filterEnableAttr.Value = filter.IsEnable.ToString();
+                filterElement.Attributes.Append(filterEnableAttr);
+
+                filterElement.InnerText = filter.FilterString;
+                filtersElement.AppendChild(filterElement);
+            }
+
+            return filtersElement;
+        }
+    }
+}
---
+++ source/VSArrange/Filter/ItemAttachmentFilter.cs
@@ -0,0 +1,102 @@
+﻿#region Copyright
+/*
+ * Copyright 2005-2009 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using VSArrange.Config;
+
+namespace VSArrange.Filter
+{
+    /// <summary>
+    /// プロジェクト要素登録フィルタリングクラス
+    /// </summary>
+    public class ItemAttachmentFilter
+    {
+        /// <summary>
+        /// フィルターリスト
+        /// </summary>
+        private readonly IList<Regex> _filterList;
+
+        public ItemAttachmentFilter()
+        {
+            _filterList = new List<Regex>();
+        }
+
+        /// <summary>
+        /// フィルター条件となる文字列を追加
+        /// </summary>
+        /// <param name="fileterString"></param>
+        public void AddFilter(string fileterString)
+        {
+            if(fileterString == null)
+            {
+                throw new ArgumentNullException("filterString");
+            }
+
+            _filterList.Add(new Regex(fileterString, RegexOptions.IgnoreCase));
+        }
+
+        /// <summary>
+        /// フィルター条件となる文字列を追加
+        /// </summary>
+        /// <param name="configInfoFilters"></param>
+        public void AddFilters(IList<ConfigInfoFilter> configInfoFilters)
+        {
+            if (configInfoFilters == null)
+            {
+                throw new ArgumentNullException("configInfoFilters");
+            }
+
+            foreach (ConfigInfoFilter filter in configInfoFilters)
+            {
+                if(filter.IsEnable &&
+                    !string.IsNullOrEmpty(filter.FilterString))
+                {
+                    AddFilter(filter.FilterString);
+                }
+            }
+        }
+
+        /// <summary>
+        /// フィルターを消去する
+        /// </summary>
+        public void Clear()
+        {
+            _filterList.Clear();
+        }
+
+        /// <summary>
+        /// 引数の文字列がフィルターを全て通過するか判定
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public bool IsPassFilter(string target)
+        {
+            foreach (Regex regex in _filterList)
+            {
+                if(regex.IsMatch(target))
+                {
+                    //  一つでも引っかかったらアウト
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
