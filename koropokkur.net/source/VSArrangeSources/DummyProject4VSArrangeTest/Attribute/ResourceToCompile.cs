b2ad26f0e07f92e9772738ad3c5f6dbf54715d6b
koyak <keikotani926@gmail.com>
2011-05-02 00:29:18 +0000

    動作確認用プロジェクト追加
    git-svn-id: https://www.seasar.org/svn/sandbox/koropokkur.net/trunk@156 34ecb7be-c3dc-40e9-8272-068907805f0b

---
+++ koropokkur.net/source/VSArrangeSources/DummyProject4VSArrangeTest/Attribute/ResourceToCompile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DummyProject4VSArrangeTest.Attribute
+{
+    /// <summary>
+    /// VSArrange実行後「ビルドアクション」が「埋め込みリソース」→「コンパイル」に
+    /// なっていることを想定しているクラス
+    /// </summary>
+    class ResourceToCompile
+    {
+    }
+}
