d33b82870adb9680544a98282a4c6375ab3b8f71
koyak <keikotani926@gmail.com>
2011-05-02 08:04:00 +0000

    ver.0.2.5リリース準備
    git-svn-id: https://www.seasar.org/svn/sandbox/koropokkur.net/tags/koropokkur.net_0.2.5@211 34ecb7be-c3dc-40e9-8272-068907805f0b

--- koropokkur.net/releaseTools/svnCopyAndExport.bat
+++
@@ -1,4 +0,0 @@
-cd /d %0\..
-rd /s export
-svn copy https://www.seasar.org/svn/sandbox/koropokkur.net/trunk/koropokkur.net https://www.seasar.org/svn/sandbox/koropokkur.net/tags/koropokkur.net_0.2.5 -m "ver0.2.5ÉäÉäÅ[ÉX"
-svn export https://www.seasar.org/svn/sandbox/koropokkur.net/tags/koropokkur.net_0.2.5 ./export
--- koropokkur.net/source/copy2build_VSArrange.bat
+++
@@ -1,6 +0,0 @@
-cd /d %0\..
-copy /-Y VSArrangeSources\VSArrangeConsole\bin\Release\AddInCommon.dll ..\build\2010\AddInCommon.dll
-copy /-Y VSArrangeSources\VSArrangeConsole\bin\Release\VSArrange.dll ..\build\2010\VSArrange.dll
-copy /-Y VSArrangeSources\VSArrangeConsole\bin\Release\VSArrangeConsole.exe ..\build\2010\VSArrangeConsole.exe
-copy /-Y VSArrangeSources\VSArrangeConfig\bin\Release\VSArrangeConfig.exe ..\build\2010\VSArrangeConfig.exe
-pause
