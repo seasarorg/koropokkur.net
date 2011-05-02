81373f9e9f8dbb57f7a93a12ce8414d1b820c074
koyak <keikotani926@gmail.com>
2011-05-02 06:07:20 +0000

    ver.0.2.5リリース準備
    git-svn-id: https://www.seasar.org/svn/sandbox/koropokkur.net/trunk/koropokkur.net@184 34ecb7be-c3dc-40e9-8272-068907805f0b

---
+++ releaseTools/mv2dl.bat
@@ -0,0 +1,4 @@
+cd /d %0\..
+move export\koropokkur.net_0.2.5.zip E:\source\seasar\www_koropokkur\download\koropokkur.net_0.2.5.zip
+svn add E:\source\seasar\www_koropokkur\download\koropokkur.net_0.2.5.zip
+svn commit E:\source\seasar\www_koropokkur\download\koropokkur.net_0.2.5.zip -m "ver0.2.5ÉäÉäÅ[ÉX"
